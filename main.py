eab521ba8065e9819ee851565f7bc24ff0f3d861
0xsamuel-eth <sammmastenn@gmail.com>
2023-09-07 13:46:50 -0400

    Added functions to create the query and url for the search and a list of valid websites:

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import webbrowser
 import sys
 
-url = 'http://docs.python.org/'
+url = 'https://www.google.com/search?q='
+
+valid_websites = [
+    'reddit.com',
+    'stackoverflow.com',
+    'stackexchange.com',
+    'medium.com'
+]
 
 # MacOS
 chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 
-print(sys.argv[1:])
+# Windows
+# chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
-webbrowser.get(chrome_path).open(url)
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+def create_filter():
+    filter = '('
+    for index, website in enumerate(valid_websites):
+        filter += 'site:' + website
+        if index == len(valid_websites) - 1:
+            filter += ')'
+
+def create_query():
+    query = sys.argv[1:]
+    return ' '.join(query)
+
+def create_url():
+    return url + create_query()
+
+print(create_url())
+
+# webbrowser.get(chrome_path).open(url)
