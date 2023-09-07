a88b1579424fc979b320c23b49c0f69a6c454841
0xsamuel-eth <sammmastenn@gmail.com>
2023-09-07 13:25:19 -0400

    Import sys and extract search keywords while splicing out file call (main.py)

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import webbrowser
+import sys
 
 url = 'http://docs.python.org/'
 
 # MacOS
 chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 
-# Windows
-# chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-
-# Linux
-# chrome_path = '/usr/bin/google-chrome %s'
+print(sys.argv[1:])
 
 webbrowser.get(chrome_path).open(url)
